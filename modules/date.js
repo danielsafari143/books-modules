3ac43b43edf5b84d004c4b7fc521b5baf9191bfc
danielsafari143 <danielsafari143@gmail.com>
2023-05-02 01:52:11 +0200

    change the dates function into arrow function

--- modules/date.js
+++ modules/date.js
@@ -1,6 +1,6 @@
 import { DateTime } from '../node_modules/luxon/src/luxon.js';
 
-function dates() {
+const dates = () => {
   const saha = DateTime.now();
   const formats = `${saha.monthLong} ${saha.day} ${saha.year} / ${saha.hour}:${saha.minute}:${saha.second} `;
   document.getElementById('currentDate').innerHTML = formats;
