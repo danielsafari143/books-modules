9b9c79bf58353e629d3bab94543a19d70d6ea5cf
danielsafari143 <danielsafari143@gmail.com>
2023-05-02 01:24:11 +0200

    changes

--- modules/books.js
+++
@@ -1,6 +0,0 @@
-const localBooks = (books) => {
-  const storage = JSON.parse(localStorage.getItem('Books'));
-  return storage || books;
-};
-
-export default localBooks;
--- modules/date.js
+++
@@ -1,9 +0,0 @@
-import { DateTime } from '../node_modules/luxon/src/luxon.js';
-
-function dates() {
-  const saha = DateTime.now();
-  const formats = `${saha.monthLong} ${saha.day} ${saha.year} / ${saha.hour}:${saha.minute}:${saha.second} `;
-  document.getElementById('currentDate').innerHTML = formats;
-}
-
-export default dates;
