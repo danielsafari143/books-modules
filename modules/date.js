d919fa50848d787d431253605f426462ee3baacb
danielsafari143 <danielsafari143@gmail.com>
2023-05-02 01:56:12 +0200

    fix eslint errors in modules

--- modules/date.js
+++ modules/date.js
@@ -4,6 +4,6 @@ const dates = () => {
   const saha = DateTime.now();
   const formats = `${saha.monthLong} ${saha.day} ${saha.year} / ${saha.hour}:${saha.minute}:${saha.second} `;
   document.getElementById('currentDate').innerHTML = formats;
-}
+};
 
 export default dates;
